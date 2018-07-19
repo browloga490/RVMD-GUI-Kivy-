29b33a10bb8e6ab7184fbcba8eb062513aa14488
Logan Brown <lbrow077@uottawa.ca>
2018-07-19 11:57:26 -0400

    Created tabs for the four channels - each tab has two screens.

--- TestingGround.py
+++ TestingGround.py
@@ -7,6 +7,11 @@ from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 
+from kivy.config import Config
+Config.set('graphics', 'width', '1024')
+Config.set('graphics', 'height', '600')
+Config.write()
+#Window.size = (1024, 600)
 
 class ScreenOne(Screen):
     pass
@@ -14,7 +19,16 @@ class ScreenOne(Screen):
 class ScreenTwo(Screen):
     pass
 
-class ScreenManagement_1(ScreenManager):
+class Channel_1(ScreenManager):
+    pass
+
+class Channel_2(ScreenManager):
+    pass
+
+class Channel_3(ScreenManager):
+    pass
+
+class Channel_4(ScreenManager):
     pass
 
 class CalcGridLayout(GridLayout):
@@ -26,7 +40,7 @@ class OptGridLayout(GridLayout):
 class Test(TabbedPanel):
 
     tab_pos = 'top_mid'
-    tab_width = Window.width / 3
+    tab_width = Window.width / 4
   
 
 presentation = Builder.load_file("main.kv")
